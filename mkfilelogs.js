3cadd3c1890a2528205193e472306dea17da59ea
max.white <max.beliy2011@mail.ru>
2022-10-07 19:40:19 +0300

    lesson-3

---
+++ mkfilelogs.js
@@ -0,0 +1,13 @@
+import * as readline from 'node:readline';
+import * as fs from 'fs';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
